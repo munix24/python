860d7576c7c9c31a8e2c657b7af8ec4670b20fdd
munix24 <munix24@gmail.com>
2017-01-27 18:06:45 -0500

    Update VB6 count sub lines.py

--- VB6 count sub lines.py
+++ VB6 count sub lines.py
@@ -3,11 +3,11 @@ path='./'        #'./'for cur dir
 #listing = os.listdir(path)                                     #another dir method
 #for infile in listing:
 with open('linecount.csv', 'wt') as out_file:                   #open output file (cur dir) for write. creates file if it doesnt exist
-    out_file.write('filename, start,end,length,sub,ave len/sub\n')       #write headings to output
+    out_file.write('filename, start,end,length,sub,avg len/sub\n')       #write headings to output
     iFileTot=0                                                  #file total  
     iLineTot=0                                                  #line total per file
-    iLineLenTot=0                                               #line length total within strings
-    iStringTot=0                                                #string count total
+    iSubLenTot=0                                               	#sub length total
+    iSubCntTot=0                                                #sub count total
     for filename in glob.glob(os.path.join(path, '*.*')):       #loop through all files in cur dir
         try:
             lFileTypeSkip = ['.py', 'dsr', 'dsx','frx','csv']   #skip file extensions
@@ -15,13 +15,13 @@ with open('linecount.csv', 'wt') as out_file:                   #open output fil
             if any(x in filename[-3:].lower() for x in lFileTypeSkip): 
                 continue                                        #skip file
             out_file.write(filename+'\n')                       #write filename to output
-            iLineCur=0                                          #line cur
-            iLineStart=0                                        #line start
+            iLineCur=0                                          #line cur num
+            iLineStart=0                                        #line start num
             
-            iStringFil=0                                        #string count in file
-            iLineLen=0                                          #line length
-            iLineLenFil=0                                       #line length cumulative
-            sStringName=''                                      #string name
+            iSubCnt=0                                        		#sub count in file
+            iSubLen=0                                          	#sub length
+            iSubLenFil=0                                       	#sub length in file
+            sSubName=''                                      		#sub name
             lStringStart = ['Private Function', 'Public Function', 'Private Sub','Public Sub']  #string matches to begin counting
             lStringEnd   = ['End Function', 'End Sub']                                          #string matches to end counting
             with open(filename) as file:                        
@@ -29,29 +29,29 @@ with open('linecount.csv', 'wt') as out_file:                   #open output fil
                     iLineCur+=1                                 #store file line num
                     #if any(x in line for x in lStringStart):   #if any strings are found in line
                     if line.startswith(tuple(lStringStart)):    #if any strings are found in beg of line
-                        sStringName=line[:line.find('(')]       #method name (from beggining of line to first '('
+                        sSubName=line[:line.find('(')]       #method name (from beggining of line to first '('
                         iLineStart=iLineCur                     #store start line num
-                    if iLineStart !=0 and line.startswith(tuple(lStringEnd)):    
-                        iLineLen=iLineCur-iLineStart+1          #+1 for inclusive
-                        iStringFil+=1                           #inc string count by 1
-                        iLineLenFil+=iLineLen                   #string line length for file
-                        out_file.write(',' + str(iLineStart) + ',' + str(iLineCur) + ',' + str(iLineLen)+','+sStringName +'\n')
+                    if iLineStart !=0 and line.startswith(tuple(lStringEnd)):   
+                        iSubCnt+=1                           		#inc string count by 1 
+                        iSubLen=iLineCur-iLineStart+1          	#+1 for inclusive
+                        iSubLenFil+=iSubLen                   	#string line length for file
+                        out_file.write(',' + str(iLineStart) + ',' + str(iLineCur) + ',' + str(iSubLen)+','+sSubName +'\n')
                         iLineStart=0                            #reset start line num
-                if(iLineCur!=0 and iLineLenFil!=0 and iStringFil!=0):
-                    out_file.write('Total:,,'+ str(iLineCur) +','+str(iLineLenFil)+','+str(iStringFil)+ ','+str(iLineLenFil/iStringFil)+'\n')
+                if(iLineCur!=0 and iSubLenFil!=0 and iSubCnt!=0):
+                    out_file.write('Total:,,'+ str(iLineCur) +','+str(iSubLenFil)+','+str(iSubCnt)+ ','+str(iSubLenFil/iSubCnt)+'\n')
                     iFileTot+=1                                 #inc file total by 1
                     iLineTot+=iLineCur                          #line count for all files
-                    iStringTot+=iStringFil                      #string count for all files
-                    iLineLenTot+=iLineLenFil                    #string line length for all files
+                    iSubCntTot+=iSubCnt                      		#string count for all files
+                    iSubLenTot+=iSubLenFil                    	#string line length for all files
         except UnicodeDecodeError:                              #could not read file so skip it
             continue
     if(iFileTot!=0):
         out_file.write('\n')
-        out_file.write(',,Tot lines,Tot length,Tot sub,ave len/sub\n')       #write headings to output
-        out_file.write(',,'+ str(iLineTot)+','+str(iLineLenTot)+','+str(iStringTot) +','+str(iLineLenTot/iStringTot)+',\n')
+        out_file.write(',,Tot lines,Tot length,Tot sub,avg len/sub\n')       #write headings to output
+        out_file.write(',,'+ str(iLineTot)+','+str(iSubLenTot)+','+str(iSubCntTot) +','+str(iSubLenTot/iSubCntTot)+',\n')
         out_file.write('\n')
-        out_file.write('File Total,,lines/file,line length/file,sub/file\n') #write headings to output
-        out_file.write(str(iFileTot)+',,'+ str(iLineTot/iFileTot)+','+str(iLineLenTot/iFileTot)+','+str(iStringTot/iFileTot) +',\n')
+        out_file.write('File Total,,lines/file,sub length/file,sub/file\n') #write headings to output
+        out_file.write(str(iFileTot)+',,'+ str(iLineTot/iFileTot)+','+str(iSubLenTot/iFileTot)+','+str(iSubCntTot/iFileTot) +',\n')
 print('\n')
 print('done')
 input()
