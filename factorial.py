e58da2dcdcf4c5cbd939078c75a0a090a1b32218
munix24 <munix24@gmail.com>
2017-06-15 18:39:32 -0400

    Add files via upload

--- factorial.py
+++ factorial.py
@@ -1,4 +1,3 @@
-#https://www.codechef.com/problems/FCTRL2
 import cProfile
 import operator
 import math
@@ -6,7 +5,10 @@ import math
 f=math.factorial
 
 def factIter(n):
-    return reduce(operator.mul,range(1,n+1),1)
+    num=1
+    for i in range(2,n+1):
+        num=num*i
+    return num
 
 def factIteration(n):
     num = 1
@@ -15,22 +17,57 @@ def factIteration(n):
         n = n - 1
     return num
 
-def factRecursion(n):         #NOT tail recursive
+def factRecursion(n):        #NOT tail recursive
     return 1 if n<=1 else n * factRecursion(n-1)
-
-def factLERecursion(n,prod):  #NOT tail recursive
+	
+def factLERecursion(n,m=1):  #NOT tail recursive
     if n<=1:
-        return prod
+        return m
     else: 
-        prod=(n-1)*prod
-        return factLERecursion(n-1,prod)
+        return factLERecursion(n-1,n*m)
 
-def factLBRecursion(n,i=1,prod=1):  #NOT tail recursive
-    if n<i:
-        return prod
-    else: 
-        prod=i*prod
-        return factLBRecursion(n,i+1,prod)
+#BEGIN TAIL RECURSION
+#http://www.kylem.net/programming/tailcall.html
+class TailCaller(object) :
+    def __init__(self, f) :
+        self.f = f
+    def __call__(self, *args, **kwargs) :
+        ret = self.f(*args, **kwargs)
+        while type(ret) is TailCall :
+            ret = ret.handle()
+        return ret
 
-#cProfile.run("factIteration(100000)")
-print(factIteration(5))
+class TailCall(object) :
+    def __init__(self, call, *args, **kwargs) :
+        self.call = call
+        self.args = args
+        self.kwargs = kwargs
+    def handle(self) :
+        if type(self.call) is TailCaller :
+            return self.call.f(*self.args, **self.kwargs)
+        else :
+            return self.call(*self.args, **self.kwargs)
+
+@TailCaller
+def factTailRecursion(n, r=1) :
+    if n <= 1 :
+        return r
+    else :
+        return TailCall(factTailRecursion, n-1, n*r)
+
+#cProfile.run('factTailRecursion(20000)')
+print(factIter.__code__)
+
+'''
+run=factTailRecursion
+param=20000
+print(run(param))
+
+if run.__module__ =='__main__':
+    exec=run.__name__
+else:
+    exec=run.__module__+'.'+run.__name__
+exec=exec+'('+str(param)+')'
+cProfile.run(exec)
+input()
+'''
---
+++ fibo.py
@@ -0,0 +1,28 @@
+#https://www.codechef.com/problems/FCTRL2
+import cProfile
+import operator
+import math
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+    
+def fibm(n):
+    memo = {0:0, 1:1}
+    if not n in memo:
+        memo[n] = fibm(n-1) + fibm(n-2)
+    return memo[n]
+
+def fibi(n):
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+
+#cProfile.run("factRecursion(1000)")
+print(fibm(5))
+input()
