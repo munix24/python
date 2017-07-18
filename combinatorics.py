97ba2bf61857af170024695912ae2abb08cffae4
munix24 <munix24@gmail.com>
2017-07-17 21:16:51 -0400

    Update combinatorics.py

--- combinatorics.py
+++ combinatorics.py
@@ -7,5 +7,20 @@ f=math.factorial
 def nCr(n, k):
     return f(n)//f(k)//f(n-k)
 
+def pascal(n):
+    if n == 1:
+        return [1]
+    else:
+        line = [1]
+        previous_line = pascal(n-1)
+        for i in range(len(previous_line)-1):
+            line.append(previous_line[i] + previous_line[i+1])
+        line += [1]
+return line
+
+def listPascal(n):
+    for i in range(1,n):
+    print(pascal(i))
+
 #cProfile.run("nCr(50000,44)")
 print(nCr(500,499))
