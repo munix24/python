e4779842c469fc5496f97e130400f968900df9fb
munix24 <munix24@gmail.com>
2017-07-24 17:10:23 -0400

    Add files via upload

--- primeFactors.py
+++ primeFactors.py
@@ -1,7 +1,17 @@
 import cProfile
 from functools import reduce
 
+def factors(n):
+    '''
+            Returns list of all divisors n 
+    '''
+    step = 2 if n%2 else 1       
+    return set(reduce(list.__add__,([i, n//i] for i in range(1, int(n**0.5) + 1, step) if n % i == 0)))  
+
 def primeFactors(n):
+    '''
+            Returns list of primes that divide n 
+    '''
     fact = []
     i=2
     while n % i == 0:
@@ -19,6 +29,9 @@ def primeFactors(n):
     return fact
 
 def primeFactorsGenerator(n):   #about the same run time
+    '''
+            Generator of prime factors of n
+    '''
     i=2
     while n % i == 0:
         n //= i
@@ -31,12 +44,48 @@ def primeFactorsGenerator(n):   #about the same run time
             n //= i
             yield i
     if n > 1:
-        yield i
-
-def factors(n):     
-    step = 2 if n%2 else 1       
-    return set(reduce(list.__add__,([i, n//i] for i in range(1, int(n**0.5) + 1, step) if n % i == 0)))   
+        yield i 
         
+def cnt_prime_Factors(n):
+    '''
+            Returns dictionary of (prime, count) pairs that divide n 
+    '''
+    fact = {}
+    i=2
+    while n % i == 0:
+        n //= i
+        if i in fact:
+            fact[i]+=1
+        else:
+            fact[i]=1
+    i=3
+    while i * i <= n:
+        if n % i:
+            i += 2
+        else:
+            n //= i
+            if i in fact:
+                fact[i]+=1
+            else:
+                fact[i]=1
+    if n > 1:
+        if n in fact:
+            fact[n]+=1
+        else:
+            fact[n]=1
+    return fact
+	
+#https://en.wikipedia.org/wiki/Divisor_function#Properties
+def cnt_divisors(n):
+    '''
+            Returns the count of divisors of n, ex(28)=6 (1,2,4,7,14,28)
+    '''
+    cnt=1
+    #cnt_primes=num_prime_Factors(n)
+    for cnt_prime in cnt_prime_Factors(n).values():
+         cnt*=(cnt_prime+1)
+    return cnt
+	
 #n=100000000000000000000000000000000000000
 #n=735734573573498646511235123
 #print(len(str(n)))
@@ -45,3 +94,4 @@ def factors(n):    
 #cProfile.run('print(primeFactors(n))')
 #cProfile.run('print(primeFactorsGenerator(n))')
 #cProfile(max(primeFactors(600851475143)))
+
