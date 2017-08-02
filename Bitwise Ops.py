5e7e82b9c66962066ebc3b360935b1884db3a854
munix24 <munix24@gmail.com>
2017-08-02 14:53:15 -0400

    Add files via upload

---
+++ Bitwise Ops.py
@@ -0,0 +1,13 @@
+#https://stackoverflow.com/questions/10670379/find-xor-of-all-numbers-in-a-given-range
+#https://codereview.stackexchange.com/questions/153262/calculate-xor-of-many-consecutive-and-non-consecutive-numbers
+#https://stackoverflow.com/questions/45335975/trying-to-complete-google-foo-bar-level-3-queue-to-do-and-keep-exceeding-time
+
+def f(n):
+    #XOR of range repeats after 4 numbers
+    return [n, 1, n + 1, 0][n % 4]
+
+def XorRange(beg, end):
+    """
+    Returns XOR of all values between a and b. Works bc XOR is associative and commutative
+    """
+    return f(beg - 1) ^ f(end)
