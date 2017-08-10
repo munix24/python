7bb1e69b5079b60d7d0f7462bc3b747e7dc6b3ea
munix24 <munix24@gmail.com>
2017-08-10 11:46:50 -0400

    Update stack.py

--- longest absolute path in file system/stack.py
+++ longest absolute path in file system/stack.py
@@ -1,22 +1,31 @@
-def getLongestPath(input):
-  if (input is null or input == ""): 
-    return 0;
-  list = input.splitlines();  # Parse the string into list
-  st = [-1]; #-1 to cancel the path separator before root dir
-  lastLevel = -1;  # depth of the last item in st
-  maxLen = 0;
-  for item in list:
-    bareName = item.lstrip('\t');	# Strip leading '\t's
-    curLevel = item.count('\t');
-    while (curLevel <= lastLevel): # cd .. to the same level as "item"
-      st.pop();
-      lastLevel -= 1;	
-    a=st[-1];	
-    st.append(len(bareName)+st[-1]+1);	# accumulated lenth, +1 for pathsep
-    lastLevel=curLevel;
-    if ('.' in item): 	# Only count "files" with an extension
-      maxLen = max(maxLen, st[-1]);
-  return maxLen;
+def len_longest_dir_path(path):
+    """
+    returns the length of the longest file name (including directory) in file system
+    """
+    if (not path): 
+        return 0
+    
+    st = [-1] #stack. -1 to cancel the path separator before root dir
+    last_level = -1  #depth of the last item in st
+    max_len = 0
+    
+    for line in path.splitlines(): #parse path into list and iterate
+        cur_level = line.count('\t')
+        bare_name = line.lstrip('\t')
+        
+        while (cur_level <= last_level): #cd .. to the same level as "item"
+            st.pop()
+            last_level -= 1
+            
+        st.append(len(bare_name) + st[-1] + 1) #accumulated lenth, +1 for pathsep
+        last_level = cur_level
+        
+        if ('.' in line): #Only count files with an extension
+            max_len = max(max_len, st[-1])
+    return max_len
 
-longest_dir_path("");
-longest_dir_path("dir\n\tsubdir1\n\t\tfile.ext\n\tsubdir22\n\t\tfile.ext");
+path=''
+print(len_longest_dir_path(path)) # 0
+
+path='dir\n\tsubdir1\n\t\tsubdir12\n\t\t\tfile.ext\n\tsubdir2\n\t\tsubdir22\n\t\t\tfile.ext'
+print(len_longest_dir_path(path)) # 29
