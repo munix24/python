f82787396fbbe31f4f152f798e39c2c2ac45675e
munix24 <munix24@gmail.com>
2017-08-10 11:30:36 -0400

    Update dictionary.py

--- longest absolute path in file system/dictionary.py
+++ longest absolute path in file system/dictionary.py
@@ -1,15 +1,22 @@
 def longest_dir_path(path):
+    """
+    returns the length of the longest file name (including directory) in file system
+    """
     longest_so_far = -1
     height_at_level = {-1: 0}
     level = 0
 
     for line in path.split('\n'):
+        #tabs, '\t', specifies the directory level. For example 0 '\t' means root
         level = line.count('\t');
         string = line.lstrip('\t')
+        #if file
         if '.' in string:
             longest_so_far = max(longest_so_far, len(string) + height_at_level[level-1])
+        #if directory add 1 to len(string) for suffix, '/'
         else:
             height_at_level[level] = (height_at_level[level-1] + len(string) + 1)
     return longest_so_far
 
-longest_dir_path("dir\n\tsubdir1\n\t\tsubdir2\n\t\t\tfile.ext\n\tsubdir22\n\t\tfile.ext");
+path='dir\n\tsubdir1\n\t\tsubdir12\n\t\t\tfile.ext\n\tsubdir2\n\t\tsubdir22\n\t\t\tfile.ext'
+print(longest_dir_path(path)) #29
