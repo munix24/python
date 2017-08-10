0036f1f30cd495200fb8d191745c5ceff942dde2
munix24 <munix24@gmail.com>
2017-08-10 11:51:08 -0400

    Update and rename lcm.py to gcd and lcm.py

---
+++ gcd and lcm.py
@@ -0,0 +1,5 @@
+#from fractions import gcd
+
+def gcd(a, b): return a if b == 0 else gcd(b,a%b)
+
+def lcm(a, b): return a * b / gcd(a, b)
--- lcm.py
+++
@@ -1,2 +0,0 @@
-from fractions import gcd
-def lcm(a, b): return a * b / gcd(a, b)
