cc520912aec57c3109a08f692cf2eff1bcc604e6
munix24 <munix24@gmail.com>
2017-09-03 11:03:57 -0400

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -1,27 +1,43 @@
-def permute(elements):
-    """recursively generate list of all possible permutations"""
-    if len(elements) <=1:
-        yield elements
+def permute(lst):
+    """
+    recursively generate sorted list of all possible permutations of lst
+    permute([2,3]): choose 2, insert 2 before permute([3]), yield [2,3]
+    permute([2,3]): choose 3, insert 3 before permute([2]), yield [3,2]
+    permute([1,2,3]): choose 1, insert 1 before permute([2,3]), yield [1,2,3], [1,3,2]
+    permute([1,2,3]): choose 2, insert 2 before permute([1,3]), yield [2,1,3], [2,3,1]
+    permute([1,2,3]): choose 3, insert 3 before permute([1,2]), yield [3,1,2], [3,2,1]
+    """
+    if len(lst) <= 1:
+        yield lst
     else:
-        for perm in permute(elements[1:]):
-            for i in range(len(elements)):
-                # nb elements[0:1] works in both string and list contexts
-                yield perm[:i] + elements[0:1] + perm[i:]
-                
+        # choose an element in lst
+        for i in range(len(lst)):
+           # permute the list excluding the chosen element
+           for perm in permute(lst[:i] + lst[i+1:]):
+               # Insert chosen element into first position of permutation
+               yield [lst[i]] + perm
+
 def permute2(lst):
-    """recursively return list of all possible permutations"""
-    if len(lst) == 0:
-        return []
- 
-    if len(lst) == 1:
-        return [lst]
- 
-    l = [] 
- 
-    for i in range(len(lst)):
-       m = lst[i]
-       tempLst = lst[:i] + lst[i+1:]
-       
-       for p in permute2(tempLst):
-           l.append([m] + p)
-    return l
+    """
+    recursively generate list of all possible permutations of lst. Returned list is out of sort.
+    permute([2,3]): permute([3]) yields [3], insert 2 before and after 3 and yield [2,3], [3,2]
+    permute([1,2,3]): permute([2,3]): yields [2,3], [3,2] (see above)
+    permute([1,2,3]): insert 1 at every position of [2,3] yielding [1, 2, 3], [2, 1, 3], [2, 3, 1]
+    permute([1,2,3]): insert 1 at every position of [3,2] yielding [1, 3, 2], [3, 1, 2], [3, 2, 1]
+    """
+    if len(lst) <=1:
+        yield lst
+    else:
+        # loop through every permutation in lst except for first element
+        for perm in permute2(lst[1:]):
+            # loop through every position in permutation
+            for i in range(len(lst)):
+                # Insert the first element into every position of permutation
+                # equivalent to return perm.insert(i, lst[0])
+                yield perm[:i] + [lst[0]] + perm[i:]
+                
+for a in permute(range(2,4)):
+    print(a)
+    
+#for a in permute2(range(1,4)):
+#    print(a)
