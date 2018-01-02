9c933bb1d35c9e1eec019adde5394ceb1c1119b2
munix24 <munix24@gmail.com>
2018-01-02 12:34:13 -0500

    Create Turtle graphic.py

---
+++ misc/Turtle graphic.py
@@ -0,0 +1,13 @@
+import turtle
+
+tess = turtle.Turtle()       
+# Create alexalex = turtle.Turtle()       
+# Create alexalex.speed(0)
+tess.speed(0)
+
+for i in range(500): # this "for" loop will repeat these functions 500 times    
+  tess.forward(i)    
+  tess.left(91)    
+  alex.forward(i)    
+  alex.right(91)
+
