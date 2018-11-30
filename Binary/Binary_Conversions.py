7850745dd13ede39e2c049f3c11521d2d7c938f0
munix24 <munix24@gmail.com>
2018-11-30 16:36:03 -0500

    Update Binary_Conversions.py

--- Binary/Binary_Conversions.py
+++ Binary/Binary_Conversions.py
@@ -3,42 +3,44 @@
 def space_per_interval(string, interval):
     return ' '.join([string[i:i+interval] for i in range(0, len(string), interval)])
 
-def chr2ascii(char):
+def chr_2_ascii(char):
     #'c' returns 99
     return ord(char)
 
-def chr2bin(char):
+def chr_2_bin(char):
     #'c' returns '0b1100011'
     return bin(ord(char))
 
-def str2bin(string):
+def str_2_bin(string):
     #'cc' returns '11000111100011'
     #another method is ''.join('{0:08b}'.format(ord(c), 'b') for c in string)
     return ''.join(bin(ord(c))[2:].zfill(8) for c in string)
 
-def str2ascii(string):
+def str_2_ascii(string):
     #'cc' returns '99 99'
     return list(ord(c) for c in string)
 ##    return ' '.join(str(ord(c)) for c in string)
 
-def bin2ascii(bin):
+def bin_2_ascii(bin):
     #'0b1100011' returns 99
     return int(bin, 2)
 
-def bin2chr(bin):
+def bin_2_chr(bin):
     #'0b1100011' returns 'c'
     return chr(int(bin, 2))
 
-def binstr2int(binstring):
+def binstr_2_int(binstring):
     """
-    convert string of bits to integer. '0110001101100011' returns 99 99. binstring has to be length 8 with no prefix
+    convert string of bits to integer. '0110001101100011' returns 99 99. 
+    binstring has to have a length that is a multiple of 8 and no prefix
     """
     string_blocks = (binstring[i:i+8] for i in range(0, len(binstring), 8))
     return ' '.join(str(int(char, 2)) for char in string_blocks)
 
-def binstr2str(binstring):
+def binstr_2_str(binstring):
     """
-    convert string of bits to equivalent string. '0110001101100011' returns 'cc'. binstring has to be length 8 with no prefix
+    convert string of bits to equivalent string. '0110001101100011' returns 'cc'. 
+    binstring has to have a length that is a multiple of 8 and no prefix
     """
     string_blocks = (binstring[i:i+8] for i in range(0, len(binstring), 8))
     return ''.join(chr(int(char, 2)) for char in string_blocks)
@@ -48,7 +50,7 @@ def pad_bits(bits, pad = 8):
     assert len(bits) <= pad
     return [0] * (pad - len(bits)) + bits
 
-def int_to_bin(n):
+def int_2_bin(n):
     """converts integer to bit list"""
     result = []
     if n == 0:
@@ -58,11 +60,36 @@ def int_to_bin(n):
         n = n // 2
     return result
 
-def int_to_bin_rec(n):
+def int_2_bin_rec(n):
     """converts integer to bit list using recursion"""
     if n == 0:
         return []
     return int_to_bin_rec(n // 2) + [n % 2]
+
+def dec_to_base_rec(n, base):
+    '''
+    converts decimal to bit list in input base using recursion
+    example: dec_to_base_rec(.390625, 2) = [0, 1, 1, 0, 0, 1]
+    .390625 in base 2 is .011001
+    -2, -3, and -6 bits are present so calculation is:
+    2 ** -2 + 2 ** -3 + 2 ** -6 = .390625
+    '''
+    if n == 0 or n >= 1:
+        return []
+    n2 = n * base #
+ 
+    # left of decimal + dec_to_bin_rec(right of decimal)
+    return [int(n2 // 1)] + dec_to_bin_rec(n2 % 1) 
+
+def dec_to_bin_rec(n):
+    '''
+    converts decimal to bit list in base 2 using recursion
+    example: dec_to_bin_rec(.390625) = [0, 1, 1, 0, 0, 1]
+    .390625 in base 2 is .011001
+    -2, -3, and -6 bits are present so calculation is:
+    2 ** -2 + 2 ** -3 + 2 ** -6 = .390625
+    '''
+    return dec_to_base_rec(n, 2)
 
 ##print(chr2bin('c'))
 
