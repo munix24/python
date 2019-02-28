81768aa7ea187b623c90bedbe8f780cb38cfeb28
munix24 <munix24@gmail.com>
2019-02-28 16:28:25 -0500

    Update pandigital.py

--- math ops/pandigital.py
+++ math ops/pandigital.py
@@ -1,8 +1,9 @@
+def contains_1_of_each(N):
+    '''return true if N contains all digits from 1 to len(N) + 1 and no more'''
+    return set(str(N)) == set(map(str, range(1, len(str(N)) + 1)))
+
 def contains_1_of_each_digit(N, digits):
     '''return true if N contains all digits listed in digits and no more'''
     if len(str(N)) != len(digits):
         return False
-    digs = set()
-    for n in str(N):
-        digs.add(n)
-    return digs == digits
+    return set(str(N)) == digits
